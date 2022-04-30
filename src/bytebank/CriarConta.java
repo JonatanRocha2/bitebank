8f88c24ce6bc1e50d9ecfbd4bebbe00c6c61ca99
Jonatan Ivanhoe da Silva Rocha <jonatanivanhoe@gmail.com>
2022-04-30 11:33:04 -0300

    first commit

---
+++ src/bytebank/CriarConta.java
@@ -0,0 +1,23 @@
+package bytebank;
+
+public class CriarConta {
+	
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
+		
+		primeiraConta.saldo += 100;
+		System.out.println(primeiraConta.saldo);
+		
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("O saldo da primeira conta é de: R$" + primeiraConta.saldo);
+		System.out.println("O saldo da segunda conta é de: R$" + segundaConta.saldo);
+		
+		System.out.println(primeiraConta);
+	}
+
+}
